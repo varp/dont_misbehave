3d05972aad362e9924d8a48e81325f612ea02701
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2015-04-30 11:49:57 +0500

    add 404 handler

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
   def index
     unless params[:page].nil?
-      @page_num = params[:page]
+      @page_num = params[:page]   
+      unless (1..3).include? @page_num.to_i
+        not_found
+      end
     end    
+    
   end
 end
